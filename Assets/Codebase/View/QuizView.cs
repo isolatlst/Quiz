d918625910b0d5718b05ad059d8d7ddda906eb1e
Kirill <kirill.huryn.unity.dev@gmail.com>
2025-03-17 20:26:26 +0300

    add-quiz-view-logic

--- Assets/Codebase/View/QuizView.cs
+++ Assets/Codebase/View/QuizView.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using Codebase.Generic;
 using Codebase.View.Buttons;
 using QuizGameCore;
 using TMPro;
@@ -11,11 +12,43 @@ namespace Codebase.View
         [SerializeField] private TextMeshProUGUI _questionText;
         [SerializeField] private AnswerButton _answerTemplate;
         [SerializeField] private Transform _answersContainer;
-        
+        private ObjectPool<AnswerButton> _answerButtonPool;
+        private IList<AnswerButton> _activeButtons;
+        private IQuiz _currentQuiz;
+
+        private void Awake()
+        {
+            _answerButtonPool = new ObjectPool<AnswerButton>(
+                _currentQuiz.Variants.Count,
+                () =>
+                {
+                    var answ = Instantiate(_answerTemplate, _answersContainer);
+                    answ.Clicked += (string answer) =>
+                    {
+                        bool isCorrect = _currentQuiz.Answer(answer);
+                    };
+                    return answ;
+                },
+                (AnswerButton btn) => btn.gameObject.SetActive(false),
+                (AnswerButton btn) => btn.gameObject.SetActive(true)
+            );
+        }
 
         public void Render(IQuiz value)
         {
-            throw new NotImplementedException();
+            _currentQuiz = value;
+
+            foreach (var button in _activeButtons)
+                _answerButtonPool.Push(button);
+            
+            _activeButtons.Clear();
+
+            foreach (var variant in _currentQuiz.Variants)
+            {
+                var answer = _answerButtonPool.Pop();
+                _activeButtons.Add(answer);
+                answer.Render(variant);
+            }
         }
     }
 }
