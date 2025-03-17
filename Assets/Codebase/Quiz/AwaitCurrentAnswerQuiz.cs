c6034fd8d615c188b44d729de18f7a7dfda0568c
CallMeNAKS <thefan51999@gmail.com>
2025-03-17 21:20:09 +0300

    add iquiz logic

---
+++ Assets/Codebase/Quiz/AwaitCurrentAnswerQuiz.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using QuizGameCore;
+using QuizGameCore.Utils;
+
+namespace Codebase.Quiz
+{
+    public class AwaitCurrentAnswerQuiz : IQuiz
+    {
+        private IQuiz _origin;
+        private bool _isCorrect;
+        private readonly IReadOnlyList<string> _variants;
+
+        public AwaitCurrentAnswerQuiz(IQuiz origin)
+        {
+            _origin = origin;
+        }
+
+        public string Question => _origin.Question;
+        public IReadOnlyList<string> Variants => _origin.Variants;
+
+        public bool Answer(string answer)
+        {
+            if (_origin.Answer(answer).Cache(out var result))
+            {
+                _isCorrect = true;
+            }
+
+            return result;
+        }
+
+
+        public IEnumerator WaitCorrect()
+        {
+            while (_isCorrect == false)
+            {
+                yield return null;
+            }
+        }
+    }
+}
---
+++ Assets/Codebase/Quiz/TimedQuiz.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Codebase.View;
+using QuizGameCore;
+
+namespace Codebase.Quiz
+{
+    public class TimedQuiz : IQuiz
+    {
+        private readonly Timer _timer;
+        private readonly IQuiz _origin;
+        private readonly int _rewardTime;
+
+        public TimedQuiz(Timer timer, IQuiz origin, int rewardTime)
+        {
+            _timer = timer;
+            _origin = origin;
+            _rewardTime = rewardTime;
+        }
+
+        public string Question => _origin.Question;
+        public IReadOnlyList<string> Variants => _origin.Variants;
+
+        public bool Answer(string answer)
+        {
+            var result = _origin.Answer(answer);
+
+            if (result)
+            {
+                _timer.AddTime(_rewardTime);
+            }
+
+            return result;
+        }
+    }
+}
